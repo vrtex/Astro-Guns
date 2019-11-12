217aaae995aefd4496e34558c621d2d578d1861d
Rootar1 <mokrosinski95@gmail.com>
2019-11-12 08:56:09 +0100

    add interfaces

--- AstroGuns/Assets/source/inventorySystem/dragAndDrop/ItemDragSystem.cs
+++ AstroGuns/Assets/source/inventorySystem/dragAndDrop/ItemDragSystem.cs
@@ -1,9 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ItemDragSystem : MonoBehaviour
+public class ItemDragSystem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    //Vector3 pos;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //beginPosition = transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.position = Vector3.zero;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
