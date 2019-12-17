be27a582a1db1043ada96ab06d1d372f6be0e5c9
Rootar1 <mokrosinski95@gmail.com>
2019-12-17 10:01:38 +0100

    Dodanie metod do pracy na acziwmentów

--- AstroGuns/Assets/source/AchievementManager.cs
+++ AstroGuns/Assets/source/AchievementManager.cs
@@ -35,4 +35,54 @@ public class AchievementManager : MonoBehaviour
             #endif
         }
     }
+
+    // NOTE: nazwy acziwmentów znajdziecie w klasie EM_GameServicesConstants (generowana klasa automatycznie)
+
+    public void RevealAchievement(string achievementName)
+    {
+        if (GameServices.IsInitialized())
+        {
+            GameServices.RevealAchievement(achievementName);
+        }
+        else
+        {
+            #if UNITY_ANDROID
+                GameServices.Init();    // start a new initialization process
+            #elif UNITY_IOS
+                Debug.Log("The user is not logged in to Game Center.");
+            #endif
+        }        
+    }
+
+    public void UnlockAchievement(string achievementName)
+    {
+        if (GameServices.IsInitialized())
+        {
+            GameServices.UnlockAchievement(achievementName);
+        }
+        else
+        {
+            #if UNITY_ANDROID
+                GameServices.Init();    // start a new initialization process
+            #elif UNITY_IOS
+                Debug.Log("The user is not logged in to Game Center.");
+            #endif
+        }        
+    }
+
+    public void ReportIncrementalAchievementsProgress(string achievementName, float progress)
+    {
+        if (GameServices.IsInitialized())
+        {
+            GameServices.ReportAchievementProgress(achievementName, progress)
+        }
+        else
+        {
+            #if UNITY_ANDROID
+                GameServices.Init();    // start a new initialization process
+            #elif UNITY_IOS
+                Debug.Log("The user is not logged in to Game Center.");
+            #endif
+        }
+    }
 }
